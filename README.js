1d4f97773ef49bb2235e49fcf2bed0b5d9d2ed5e
Wydios <fabianhorter@gmail.com>
2025-02-05 15:31:37 +0100

    Update README.md

--- README.js
+++ README.js
@@ -1,19 +1,23 @@
-
+```js
 function About() {
     const name = 'Fabian';
     const user = 'Wydios';
     let age = 16;
     const today = new Date();
+
     const isBirthday = (date, month, day) =>
         date.getMonth() === month && date.getDate() === day;
+
     if (isBirthday(today, 9, 3)) {
         age++;
     }
+
     const socials = {
         youtube: '@wydios',
         twitch: '@wydios',
         discord: '@wydios',
         tiktok: '@wydios'
     };
-    return { name, user, age, socials, };
+
+    return { name, user, age, socials };
 };
